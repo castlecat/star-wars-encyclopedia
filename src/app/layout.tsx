06f0e321516caa4ac3a92f43be0f910b2282fa07
Karels Straujais <157560986+castlecat@users.noreply.github.com>
2024-09-18 20:06:27 +0300

    added apollo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { ApolloWrapper } from "../components/ApolloWrapper";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
   );
